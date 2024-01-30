c481c2fd13267772f3f178dac739afa4979ca18d
Martin Kirilov <wucdbm@gmail.com>
2024-01-30 17:38:08 +0200

    fix: cleanup

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -15,13 +15,6 @@ export function WucdbmViteVueSsr(options: PluginConfig = {}): PluginOption {
         },
         async configureServer(server) {
             if (process.env.DEV_SSR) {
-                if (options.polyfills !== false && !globalThis.fetch) {
-                    const fetch = await import('node-fetch')
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
-                    globalThis.fetch = fetch.default || fetch
-                }
-
                 if (false === options.ssr?.nodeTlsRejectUnauthorized) {
                     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
                 }
